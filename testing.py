827d31545836b9261de8db9833fae6b9f3a88e2d
sxdny <67187745+sxdny@users.noreply.github.com>
2025-09-30 20:21:02 +0200

    new project added: thread shed

--- testing.py
+++ testing.py
@@ -1,25 +1,15 @@
 # Testing file ;)
 
-# from datetime import datetime
-
-# # Hora en formato de 24 horas
-# hora_24 = 15
-
-# # Convertir a formato de 12 horas
-# hora_12 = datetime.strptime(f"{hora_24}", "%H").strftime('%-I%p')
-
-# print(hora_12)  # Salida: 03 PM
-
-items = {'pancakes': 7.50, 'waffles': 5.50, 'burguer': 11.00}
-
-for key, value in items.items():
-    print(key, value)
+def contains(big_string, little_string) -> bool:
+  if little_string in big_string:
+    return True
+  else:
+    return False
 
 
-
-def calculate_total(purchased_items):
-    items_eq = [value for key, value in items.items() if key in purchased_items]
-    print(sum(items_eq))
-        
-
-calculate_total(['waffles', 'pancakes'])
+def common_letters(string_one, string_two) -> list:
+  list_common = []
+  for char in string_one:
+    if char in string_two and not char in list_common:
+      list_common.append(char)
+  return list_common
